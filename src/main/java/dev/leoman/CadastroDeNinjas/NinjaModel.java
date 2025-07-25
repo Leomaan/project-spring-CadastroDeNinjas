4d44775c9aa3c82b972345ef793da07684e5cd04
Leomaan <146137607+Leomaan@users.noreply.github.com>
2025-07-25 17:40:27 -0300

    Entidade criada no banco de dados

---
+++ src/main/java/dev/leoman/CadastroDeNinjas/NinjaModel.java
@@ -0,0 +1,51 @@
+package dev.leoman.CadastroDeNinjas;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_cadastro")
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String email;
+    private int age;
+
+    public NinjaModel() {}
+
+    public NinjaModel(String name, int age, String email) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
